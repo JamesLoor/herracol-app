805ab69b2de9199485126f0e8467ac4816e6c86b
DerianPaez <derianpaez01@hotmail.com>
2023-05-12 17:00:28 -0500

    Setup styles and theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#091254",
+        primaryDark: "#20166e",
+        primaryLight: "#100d28",
+        secundaryDark: "#797979",
+        secundaryLight: "#C4C4C4",
+        accent: "#E8DC00",
+        link: "#1101A6",
+        white: "#FFFFFF",
+        black: "#222931",
+        error: "#FF5252",
+        modalBackground: "rgba(0, 0, 0, 0.3)",
+        whatsapp: "#25D366",
+        ok: "#47C9A2",
+        skeleton: "#E3E3E3",
+      },
+      gridTemplateColumns: {
+        header: "max-content max-content",
       },
     },
   },
   plugins: [],
-}
+};
