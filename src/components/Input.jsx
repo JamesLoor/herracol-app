f5d14abe24a535010a29cdbf022fb30512ea822e
james.mendoza <james.mendoza@rappi.com>
2023-05-19 19:14:39 -0500

    Add input and Textarea component

---
+++ src/components/Input.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Input({ label, name, onChange, type = "text" }) {
+  return (
+    <label className="grid gap-1">
+      {label}
+      <input
+        className="w-full h-12 px-5 rounded-[20px] border-solid border-secundaryLight border"
+        type={type}
+        name={name}
+        onChange={onChange}
+      />
+    </label>
+  );
+}
---
+++ src/components/Textarea.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function Textarea({
+  label,
+  name,
+  onChange,
+  rows = 4,
+  cols = null,
+}) {
+  return (
+    <label className="grid gap-1">
+      {label}
+      <textarea
+        cols={cols}
+        rows={rows}
+        className="w-full px-5 py-3 rounded-[20px] border-solid border-secundaryLight border resize-none overflow-hidden"
+        name={name}
+        onChange={onChange}
+      />
+    </label>
+  );
+}
