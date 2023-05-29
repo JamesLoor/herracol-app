79eb75d29eafe7f61ccf9038ddc84cb86876ea1a
DerianPaez <derianpaez01@hotmail.com>
2023-05-28 23:23:27 -0500

    Fix nav item onClose and active

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -3,11 +3,11 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function NavItem({ route, label }) {
+export default function NavItem({ route, label, handleClicked }) {
   const pathname = usePathname();
-  const isActive = pathname === route;
+  const isActive = pathname === route || pathname.startsWith(`${route}/`);
   return (
-    <li className="text-white text-[18px] font-bold">
+    <li onClick={handleClicked} className="text-white text-[18px] font-bold">
       <Link
         className={isActive ? "border-b-2 border-accent" : "border-b-0"}
         href={route}
