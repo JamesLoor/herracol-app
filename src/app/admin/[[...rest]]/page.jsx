7fb0b8004335175e8d2502a299aa5b85dbaaec81
DerianPaez <derianpaez01@hotmail.com>
2025-05-25 23:46:42 -0500

    Add Clerk authentication and admin layout with middleware protection

---
+++ src/app/admin/[[...rest]]/page.jsx
@@ -0,0 +1,21 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Admin() {
+  return (
+    <section className="flex justify-center items-center h-screen">
+      <SignIn
+        appearance={{
+          elements: {
+            footerAction: "hidden",
+            formButtonPrimary: "bg-primary",
+            logoImage: "w-56 h-auto",
+          },
+          layout: {
+            logoImageUrl: "/assets/logoHerracolSA.png",
+          },
+        }}
+        forceRedirectUrl="/admin/dashboard"
+      />
+    </section>
+  );
+}
