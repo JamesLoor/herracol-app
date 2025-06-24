3c0d7b91905782e45471348a571d0ab2ea7be23d
DerianPaez <derianpaez01@hotmail.com>
2025-06-24 17:18:25 -0500

    Update protected routes in middleware and remove unused auth checks in dashboard and productos pages.

--- src/app/admin/(private)/productos/page.jsx
+++ src/app/admin/(private)/productos/page.jsx
@@ -1,8 +1,5 @@
 import ProductManagement from "@/components/ProductManagement";
-import { auth } from "@clerk/nextjs/server";
 
-export default async function AdminProducts() {
-  const { userId, redirectToSignIn } = await auth();
-  if (!userId) return redirectToSignIn();
+export default function AdminProducts() {
   return <ProductManagement />;
 }
