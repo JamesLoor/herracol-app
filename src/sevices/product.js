5896d4919032c19b1b51f746d6fdcbbcbde18b34
DerianPaez <derianpaez01@hotmail.com>
2025-07-24 18:50:12 -0500

    Enhance getProducts function to handle potential null response data gracefully

--- src/sevices/product.js
+++ src/sevices/product.js
@@ -5,12 +5,14 @@ const url = process.env.NEXT_PUBLIC_FIREBASE_REALTIME_DATABASE_API;
 const getProducts = async () => {
   const response = await axios.get(`${url}/products.json`);
 
-  const products = Object.entries(response.data).map(([key, product]) => {
-    return {
-      id: key,
-      ...product,
-    };
-  });
+  const products = Object.entries(response.data || {})?.map(
+    ([key, product]) => {
+      return {
+        id: key,
+        ...product,
+      };
+    }
+  );
 
   return products;
 };
